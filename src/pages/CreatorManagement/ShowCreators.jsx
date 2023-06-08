a4ea4137e2652c3a5fc4bd1e6f6991d26ac4c3dc
Sharna Hossain <sharnajh@gmail.com>
2023-06-08 13:31:01 -0400

    lint shenanigans

--- src/pages/CreatorManagement/ShowCreators.jsx
+++ src/pages/CreatorManagement/ShowCreators.jsx
@@ -1,29 +1,21 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-key */
 import CreatorCard from "../../components/CreatorCard/CreatorCard.jsx";
 import PropTypes from 'prop-types';
 
 const ShowCreators = ({ creators }) => {
     const displayCards = () => {
-        return creators.map(creator => <CreatorCard creator={creator} />)
+        return creators.map(creator =>
+            <CreatorCard key={creator.id} creator={creator} />
+        )
     }
 
     return (
-        <>
-            <ul className="grid">
-                {creators ? displayCards() : "Loading..."}
-            </ul>
-        </>
+        <ul className="grid">
+            {creators ? displayCards() : "Loading..."}
+        </ul>
     )
 }
 ShowCreators.propTypes = {
-    creators: PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        url: PropTypes.string,
-        description: PropTypes.string,
-        imageURL: PropTypes.string
-    })
+    creators: PropTypes.array.isRequired,
 }
 
 export default ShowCreators;
