f793a7090fd33ffe120d1e8fa32af5714d2b0629
Sharna Hossain <sharnajh@gmail.com>
2023-06-15 13:36:02 -0400

    circle styling

--- src/components/FormInput/DisplayImage/DisplayImage.jsx
+++ src/components/FormInput/DisplayImage/DisplayImage.jsx
@@ -2,16 +2,22 @@ import PropTypes from 'prop-types';
 import './DisplayImage.css';
 
 const DisplayImage = ({ imageURL }) => {
-    const defaultImage = "https://placehold.co/500?text=Image+Preview&font=roboto";
     // const handleError = (el) => {
     //     el.src = "https://placehold.co/500?text=Invalid+Image+URL&font=roboto";
     // }
+    // const style = {
+    //     backgroundImage: `url(${imageURL})`
+    // }
     return (
-            <img className="photo"
-                loading="lazy"
-                alt="Image of Creator"
-                onError={({ currentTarget }) => handleError(currentTarget)}
-                src={""} />
+        // <div className="photo"
+        //     loading="lazy"
+        //     alt="Image of Creator"
+        //     style={style} >
+        //     {<h2>Image Preview</h2>}
+        // </div>
+        <div className="circle-frame">
+            {imageURL ? <img src={imageURL} /> : <h2>Image Preview</h2>}
+        </div>
     )
 }
 DisplayImage.propTypes = {
